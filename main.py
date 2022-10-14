5223bab8031671f65ff0a5558953f88a95f5d6e1
Gavin Raine Dizon <gavinrainedizon@gmail.com>
2022-10-15 00:08:28 +0800

    Added ocr preprocessing

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import string
 
 from pynput.keyboard import Listener
 import cv2
+import pytesseract
+from paddleocr import PaddleOCR, draw_ocr
 
 import hand_tracking_module as htm
 import utils as utils
 
-
+pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/5.2.0/bin/tesseract'
 
 
 
 def main():
-    if not os.geteuid() == 0:
-        sys.exit("\nOnly root can run this script\n")
+#    if not os.geteuid() == 0:
+#        sys.exit("\nOnly root can run this script\n")
 
     lm_list, lm_list2 = [], []
     detector = htm.hand_detector()
@@ -23,27 +25,54 @@ def main():
         #print("Character: {} CX: {} CY: {} HAND: {}".format(key.char, lm_list[0][1], lm_list[0][2], lm_list[0][3]))
         if len(lm_list) != 0:
             try:
-                print('alphanumeric key {0} pressed'.format(
-                    key.char))
+                print('alphanumeric key {} pressed at coordinate: {},{} at hand {} {}'.format(
+                    key.char, lm_list[4][1], lm_list[4][2], lm_list[4][3]))
             except AttributeError:
                 print('special key {0} pressed'.format(
                     key))
 
     listener = Listener(on_press=on_press, on_release=None)
     listener.start()
+    #ocr = PaddleOCR(use_angle_cls=True, lang="en",)
 
 
     cap = cv2.VideoCapture(2)
-    keys = dict.fromkeys(string.ascii_lowercase, None)
+    keys = dict.fromkeys(string.ascii_uppercase, [])
 
+    MIN = 2000
 
     #TODO: Detect Key Coordinates Placements
-    while utils.none_in_dict(keys) == True:
+    while utils.not_enough_dict(keys, MIN) == True:
         success, img = cap.read()
-        print("None")
+        h, w, c = img.shape
+
+#        img = utils.zoom(img, 2)
+
+        #grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #(thresh, grayimg) = cv2.threshold(img, 78, 230, cv2.THRESH_BINARY)
+        
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        (thresh, img) = cv2.threshold(img, 78, 230, cv2.THRESH_BINARY)
+        img = utils.canny(img)
+
+        boxes = (pytesseract.image_to_boxes(img, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 11'))
+        for b in boxes.splitlines():
+            b = b.split(' ')
+            print(b)
+            if b[0] in keys:
+                #keys[b[0]] = (int((int(b[1]) + int(b[3])) / 2), int((int(b[2]) + int(b[4])) / 2))
+                if int(b[1]) != 0 and int(b[2]) != 0 and len(keys[b[0]]) < MIN:
+                    keys[b[0]].append(utils.midpoint((int(b[1]), int(h/2) + int(b[2])), (int(b[3]), int(h/2) + int(b[4]))))
+                #img = cv2.rectangle(img, (int(b[1]), int(h) + int(b[2])), (int(b[3]), int(h) + int(b[4])), (0, 255, 0), 1)
+
+        # for key in keys.keys():
+        #     if(len(keys[key]) > 10):
+        #         print(key)
 
         cv2.imshow("Hand Camera", img)
         cv2.waitKey(1)
+
+    print(keys['A'])
 
 
     # Detect Hand Movement
@@ -52,6 +81,8 @@ def main():
         img = detector.find_hands(img)
         lm_list = detector.find_position(img)
         lm_list2 = detector.find_position(img, 1)
+        if len(lm_list) != 0:
+            print(lm_list[4])
 
         cv2.imshow("Hand Camera", img)
         cv2.waitKey(1)
--- utils.py
+++ utils.py
@@ -1,5 +1,33 @@
-def none_in_dict(d):
+import cv2 as cv
+import numpy as np
+
+def not_enough_dict(d, min = 10):
     for _, value in d.items():
-        if value is None:
+        if len(value) < min:
             return True
     return False
+
+def zoom(img, zoom_factor=2):
+    return cv.resize(img, None, fx=zoom_factor, fy=zoom_factor)
+
+def midpoint(p1, p2):
+    return ((p1[0]+p2[0])/2, (p1[1]+p2[1])/2)
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv.morphologyEx(image, cv.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv.Canny(image, 100, 200)
