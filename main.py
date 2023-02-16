f8f834e8c8dc58b8e5c3afa068d025cdb198a9ff
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2023-02-16 08:24:28 +0800

    Added salvo mapping

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ import hand_tracking_module as htm
 import utils as utils
 import keyboard_mapper as keyboard
 import numpy as np
+import json
 
 def main():
     lm_list, lm_list2 = [], []
-        #CONFIG
+    #CONFIG
     START_COORDINATE = (280,350)
     KEY_SIZE = 60
     SPACING = 10
@@ -23,13 +24,31 @@ def main():
     SALVO = ["1234567890-","DHRLWBKYEU", "STNGPMIAO;'", "ZXCVQFJ,.?"]
     TYPHE_H = ["1234567890-", "", ";'", ",.?"]
     TYPHE_LP = ["1234567890-", "", ";'", ",.?"]
-    
+
+    QWERTY_TO_SALVO =  {}
+
+    # get salvo mapping from salvo json file
+    with open('salvo.json') as json_file:
+        QWERTY_TO_SALVO = json.load(json_file)
+
+
     FILENAME_PREF = sys.argv[1] if len(sys.argv) > 0 else "DEFAULT"
+    LAYOUT = "QWERTY"
+
+    if len(sys.argv) > 2:
+        LAYOUT = str(sys.argv[2]).upper()
 
     # initialize keyboard coordinates
     keys = keyboard.create_keyboard(mapping=SALVO, starting_coordinate=START_COORDINATE, size=KEY_SIZE, spacing=SPACING)
     detector = htm.hand_detector()
     RESULT = []
+
+    def map_on_layout(raw_letter, layout = LAYOUT):
+        switcher = {
+            "SALVO": QWERTY_TO_SALVO[raw_letter],
+            "QWERTY": raw_letter
+        }
+        return switcher.get(layout, raw_letter)
 
     def on_press(key): 
         if key == Key.esc:
@@ -43,8 +62,10 @@ def main():
 
         if (len(lm_list) != 0 or len(lm_list2) != 0):
             try:
-                RESULT.append(utils.generate_object(lm_list + lm_list2, keys, key.char.upper()))
-                print(key.char.upper())
+                raw_letter = key.char.upper()
+                key_convert = map_on_layout(raw_letter)
+                RESULT.append(utils.generate_object(lm_list + lm_list2, keys, key_convert))
+                print("Pressed key: "+ key_convert)
             except AttributeError:
                 if(key == Key.space):
                     RESULT.append(utils.generate_object(lm_list + lm_list2, keys, " "))
@@ -53,7 +74,6 @@ def main():
 
     listener = Listener(on_press=on_press, on_release=None)
     listener.start()
-
 
     cap = cv2.VideoCapture(2)
 
@@ -66,28 +86,9 @@ def main():
         img = detector.find_hands(img)
         lm_list = detector.find_position(img)
         lm_list2 = detector.find_position(img, 1)
-        # if len(lm_list) != 0:
-        #     print(lm_list[8])
 
         img = cv2.putText(img, "Testing", (100, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 5)
         img = cv2.circle(img, (666, 1668), 100, (255, 0, 0), 2)
-        # img = cv2.rotate(img, cv2.ROTATE_180)
-
-        # h,  w = img.shape[:2]
-        # # New Image shape to generate
-
-        # K = np.array([[0, 0. , 0],
-        #               [0. , 50, 0],
-        #               [0. , 0., 0.]])
-
-        # # zero distortion coefficients work well for this image
-        # D = np.array([0., 0., 0., 0.])
-
-        # Knew = K.copy()
-        # Knew[(0,1), (0,1)] = 0.4 * Knew[(0,1), (0,1)]
-
-        # img = cv2.fisheye.undistortImage(img, K, D=D, Knew=Knew)
-        
         img = keyboard.show_keyboard(img, keys, starting_coordinate=START_COORDINATE, size=KEY_SIZE, spacing=SPACING)
         cv2.imshow("Hand Camera", img)
         cv2.waitKey(10)
